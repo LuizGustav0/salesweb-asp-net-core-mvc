ee20c1d7806de8512b70d1ebe7d3bda5efb552e8
LuizGustav0 <contato-luiz-gustavo@outlook.com>
2019-10-20 01:38:26 -0300

    Delete seller

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -28,6 +28,18 @@ namespace SalesWebMvc.Services
         }
 
 
+        public Seller FindByID(int id)
+        {
+            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+        }
+
+        public void Remove(int id)
+        {
+            var obj = _context.Seller.Find(id);
+            _context.Seller.Remove(obj);
+            _context.SaveChanges();
+        }
+
     }
 
  
